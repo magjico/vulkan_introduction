2b2709e49e2ca6c79f467d43d41c05cd1e5c2e03
magjico <cesar99.pujol@gmail.com>
2023-10-26 14:33:48 +0200

    Add Validation Layers

--- HelloTriangle/main.cpp
+++ HelloTriangle/main.cpp
@@ -6,9 +6,22 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <cstring>
+#include <vector>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
+const char **glfwExtensions;
+
+const std::vector<const char*> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"
+};
+
+#ifndef NDEBUG
+    const bool enableValidationLayers = false;
+#else
+    const bool enableValidationLayers = true;
+#endif
 
 class HelloTriangleApplication
 {
@@ -24,6 +37,72 @@ class HelloTriangleApplication
     private:
 
         GLFWwindow *window;
+        VkInstance instance;
+        uint32_t glfwExtensionCount;
+
+        bool checkValidationLayerSupport()
+        {
+            /**
+             * Checking if the validation layers are supported:
+             * https://vulkan-tutorial.com/en/Drawing_a_triangle/Setup/Validation_layers
+            */
+            uint32_t layerCount;
+            vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+            std::vector<VkLayerProperties> availableLayers(layerCount);
+            vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+            for (const char* layerName : validationLayers) {
+                bool layerFound = false;
+
+                for (const auto& layerProperties : availableLayers) {
+                    if (strcmp(layerName, layerProperties.layerName) == 0) {
+                        layerFound = true;
+                        break;
+                    }
+                }
+
+                if (!layerFound) {
+                    std::cout << "layer " << layerName << " was not found" << std::endl;
+                    return false;
+                }
+
+            }
+
+            return true;
+        }
+
+        void createInstance()
+        {
+            /**
+                * Create an instance to optimize our specific application.
+            */
+            if (enableValidationLayers && !checkValidationLayerSupport())
+                throw std::runtime_error("validation layers requested, but not available!");
+            
+            VkApplicationInfo appInfo{};
+            appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+            appInfo.pApplicationName = "Hello Triangle";
+            appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+            appInfo.pEngineName = "No Engine";
+            appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+            appInfo.apiVersion = VK_API_VERSION_1_0;
+
+            VkInstanceCreateInfo createInfo{};
+            createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+            createInfo.pApplicationInfo = &appInfo;
+
+            glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+            createInfo.enabledExtensionCount = glfwExtensionCount;
+            createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+            createInfo.enabledLayerCount = 0;
+
+            if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+                throw std::runtime_error("failed to create instance!"); 
+            }
+        }
 
         void initWindow() {
             /**
@@ -39,6 +118,10 @@ class HelloTriangleApplication
         }
         
         void initVulkan() {
+            /**
+             * initialize a Vulkan instance.
+            */
+            createInstance();
 
         }
 
@@ -46,15 +129,17 @@ class HelloTriangleApplication
             /**
              * Run the application.
             */
-           while ((!glfwWindowShouldClose(window))) {
+            while ((!glfwWindowShouldClose(window)))
                 glfwPollEvents();
-           }
+            
         }
 
         void cleanup() {
             /**
              * Clean up resources by destroying the GLFW application.
             */
+            vkDestroyInstance(instance, nullptr);
+
             glfwDestroyWindow(window);
 
             glfwTerminate();
